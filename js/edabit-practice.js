dc81c740ccbe0db955bf7dd7ddc4eb5c8b717e72
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-01 13:23:50 -0500

    completed two exercises

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1,1 +1,12 @@
 "use strict";
+
+//return sum of two numbers
+function addition(a, b) {
+    return a + b;
+}
+
+//convert minutes into seconds
+function convert(minutes) {
+    return minutes * 60;
+}
+
