7bd618fd91232902ce3af5bab76665d3461d52df
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-03 23:29:25 -0500

    adding function nextEdge to find maximum range of a triangle's third edge

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -36,3 +36,10 @@ function howManySeconds(hours) {
 function giveMeSomething(a) {
     return "something" + " " + a;
 }
+
+
+//Create a function that finds the maximum range of a triangle's
+// third edge, where the side lengths are all integers.
+function nextEdge(side1, side2) {
+    return (side1 + side2) - 1;
+}
