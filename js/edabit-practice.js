dccc8b06e7de3a7c6e1bdb822df5f5cf5a4f9fca
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-03 23:25:36 -0500

    adding giveMeSomething function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -30,3 +30,9 @@ function triArea(base, height) {
 function howManySeconds(hours) {
     return hours * 3600;
 }
+
+//Write a function that returns the string "something"
+// joined with a space " " and the given argument a.
+function giveMeSomething(a) {
+    return "something" + " " + a;
+}
