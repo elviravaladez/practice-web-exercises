00c17758f668c98cdf2d5e9b07ab441340626826
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-03 23:41:12 -0500

    adding lessThanOrEqualToZero function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -62,3 +62,13 @@ function findPerimeter(length, width) {
 function remainder(x, y) {
     return x % y;
 }
+
+
+//Is the Number Less than or Equal to Zero?
+
+//Create a function that takes a number as its
+// only argument and returns true if it's less than
+// or equal to zero, otherwise return false.
+function lessThanOrEqualToZero(num) {
+    return num <= 0;
+}
