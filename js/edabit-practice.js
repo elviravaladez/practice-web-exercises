905935173308b81dd201182a123adf7806c8de49
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-04 00:50:46 -0500

    adding indexOfIgnoreCase function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -167,3 +167,31 @@ function firstChar(name) {
     var trimName = name.trim();
     return trimName.charAt(0);
 }
+
+
+//String: indexOf()
+
+/*
+NOTE: To determine the first occurrence of a string within another string, use the indexOf method:
+
+var n1 = 'bit'.indexOf('it');
+var n2 = 'bit'.indexOf('js');
+var n3 = 'bit'.indexOf('IT');
+
+n1 is 1, because the starting position of 'it' in 'bit' is 1. As usual, counting starts at 0.
+indexOf returns -1 if the search string does not occur in the calling string.
+Thus n2 is -1. The indexOf method is case sensitive. So n3 is also -1.
+ */
+
+
+
+//Write a function indexOfIgnoreCase taking two strings and
+// determining the first occurrence of the second string in the first string.
+// The function should be case insensitive.
+
+// Example: indexOfIgnoreCase('bit','it') and indexOfIgnoreCase('bit','IT') should return 1.
+function indexOfIgnoreCase(str1, str2) {
+    var stringOne = str1.toLowerCase();
+    var stringTwo = str2.toLowerCase();
+    return stringOne.indexOf(stringTwo);
+}
