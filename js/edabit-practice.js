d21a6155dbfbfb5e69dca2e01daaacc64b8480c2
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-07 11:57:11 -0500

    adding is the string even or odd function using conditional

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -211,3 +211,14 @@ function isSameNum(num1, num2) {
         return false;
     }
 }
+
+
+//Is the String Odd or Even?
+//Given a string, return true if its length is even or false if the length is odd.
+function oddOrEven(s) {
+    if(s.length % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
