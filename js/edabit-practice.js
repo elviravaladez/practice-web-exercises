8100784edd25ced8f7046acb4a4b5aa09f99ade3
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-07 17:19:58 -0500

    adding isLastCharacterN function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -381,3 +381,11 @@ function acceptIntoMovie(age, isSupervised) {
 function yeah_nope(bool) {
     return bool ? "yeah" : "nope";
 }
+
+//Is the Last Character an N?
+
+// Create a function that takes a string (a random name). If the last character of
+// the name is an "n", return true, otherwise return false.
+function isLastCharacterN(word) {
+    return word.endsWith("n");
+}
