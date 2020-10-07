da09816d79f0d6cf35bd6f2145ed6eebcebd5f52
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-07 17:42:16 -0500

    practicing Even or Odd

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -449,3 +449,18 @@ function getContainer(product) {
         default:
             container = null;
     }
+}
+
+
+//Is the Number Even or Odd?
+
+//Create a function that takes a number as an argument and returns "even" for
+// even numbers and "odd" for odd numbers.
+
+function isEvenOrOdd(num) {
+    if(num % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
