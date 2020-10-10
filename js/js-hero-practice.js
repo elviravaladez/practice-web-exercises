f7c3606b5482702262ea7794fd8827e6353962f8
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 18:47:20 -0500

    adding multiple functions

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -26,3 +26,18 @@ function hello() {
     return 'Hello world!';
 }
 
+
+//Multiple Functions:
+//Define two functions. The first function a should return 'Hello a!'
+// and the second function b should return 'Hello b!'.
+
+function a() {
+    return 'Hello a!';
+}
+
+
+function b() {
+    return 'Hello b!';
+}
+
+
