541bbcfb2e233c0829a1c5151f07dde63077ccfb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 18:39:37 -0500

    declaring variables

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -1,2 +1,16 @@
 "use strict";
 console.log("JS Hero Practice Exercises");
+
+
+/*****VARIABLES********/
+
+//Declare a variable firstname and initialize it with the value 'Lata'.
+
+var firstname = 'Lata';
+
+
+//Declare a variable flower and assign it the value 'rose'. Declare a
+// second variable tree and assign it the value 'maple'.
+
+var flower = 'rose';
+var tree = 'maple';
