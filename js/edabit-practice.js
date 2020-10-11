012f979e31e3647aa5cc9c81eaed2f1885354854
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 21:08:32 -0500

    completed imposterFormula

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -567,3 +567,24 @@ function corn(num) {
 
 // console.log(corn(4));
 
+
+
+//Among Us Imposter Formula
+
+//Create a function that calculates the chance of being an imposter.
+// The formula for the chances of being an imposter is 100 × (i / p)
+// where i is the imposter count and p is the player count.
+// Make sure to round the value to the nearest integer and
+// return the value as a percentage.
+
+// The player limit is 10 and the imposter count can only go up to 3.
+
+// EXAMPLES:
+//imposterFormula(1, 10) ➞ "10%"
+// imposterFormula(2, 5) ➞ "40%"
+// imposterFormula(1, 8) ➞ "13%"
+
+function imposterFormula(i, p) {
+    var percentage = 100 * (i / p);
+    return (Math.round(percentage) + "%");
+}
