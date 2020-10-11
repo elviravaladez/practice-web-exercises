c94609fd505c70c95859774a6c043f58ee8fc8d4
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 19:08:57 -0500

    practicing with concatenation

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -61,3 +61,14 @@ var salutation = greet();
 function echo(input){
     return input;
 }
+
+
+/****** STRINGS ******/
+
+//Write a function greet having one parameter and returning 'Hello <parameter>!'.
+//Example: greet('Ada') should return 'Hello Ada!' and greet('Grace')
+// should return 'Hello Grace!'.
+
+function greet(input){
+    return 'Hello ' + input + '!';
+}
