224fae8756d05da0d5ff18e84420f71dd36ba971
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 19:13:36 -0500

    practice with logging via function log

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -72,3 +72,13 @@ function echo(input){
 function greet(input){
     return 'Hello ' + input + '!';
 }
+
+
+/******* LOGGING ******/
+
+//Write a function log that logs 'Hello Console!'. If you are working with
+// a desktop browser, open the developer tools to see the output there as well.
+
+function log() {
+    console.log('Hello Console!');
+}
