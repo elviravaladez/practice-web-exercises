d0c206daba8a5d3c48dedc4355d38295967afd10
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 19:34:36 -0500

    practicing with charAt()

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -133,4 +133,16 @@ function toCase(str) {
 
 
 
+/******* String: charAt() *******/
+
+//Write a function shortcut that takes two strings and returns
+// the initial letters of theses strings.
+//
+// Example: shortcut('Amnesty', 'International') should return 'AI'.
+
+function shortcut(str1, str2) {
+    return str1.charAt(0) + str2.charAt(0);
+}
+
+
 
