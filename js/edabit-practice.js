1a5f01e6c352f2157c2219cbef9d3cbbe547f730
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 20:57:12 -0500

    completed inchesToFeet function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -527,3 +527,25 @@ function areaShape(base, height, shape) {
 // console.log(areaShape(5, 15, "parallelogram"));
 
 
+
+
+//Inches to Feet
+
+//Create a function that accepts a measurement value in inches and
+// returns the equivalent of the measurement value in feet.
+//If inches are under 12, return 0.
+
+//EXAMPLES:
+//inchesToFeet(324) ➞ 27
+// inchesToFeet(12) ➞ 1
+// inchesToFeet(36) ➞ 3
+
+function inchesToFeet(inches) {
+    if(inches < 12) {
+        return 0;
+    }
+
+    return inches / 12;
+}
+
+console.log(console.log(inchesToFeet(360)));
