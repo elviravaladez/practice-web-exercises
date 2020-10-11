b048c75283d9f4024f7e516491cd406dd9ca0954
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 19:24:05 -0500

    practice with string length property

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -104,3 +104,16 @@ function shout(str) {
     console.log(str + str);
     return str + str;
 }
+
+
+
+/**** String: length ********/
+
+//Write a function length that takes a string and returns the number of characters of the string.
+//
+// Example: length('sun') should return 3.
+
+function length(str) {
+    return str.length;
+}
+
