d111e2ee1a85e3d646cc63426980a9c7f9dedee5
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-10 19:05:11 -0500

    practicing with parameters

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -52,3 +52,12 @@ function greet() {
 
 var salutation = greet();
 
+
+/********* PARAMETERS **********/
+
+//Write a function echo that also returns the passed parameter. echo('Greta')
+// should return 'Greta' and echo('CO2') should return 'CO2'
+
+function echo(input){
+    return input;
+}
