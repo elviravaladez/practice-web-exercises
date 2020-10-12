df37ab4d300d32689b4f6ab684888e1a7d9fbd10
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-12 17:59:14 -0500

    completed reverse an array exercise

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -796,3 +796,11 @@ function helloWorld(num) {
         return "World";
     }
 }
+
+
+
+//Reverse an Array
+//Write a function to reverse an array.
+function reverse(arr) {
+    return arr.reverse();
+}
