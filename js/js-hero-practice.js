46fea1025a7a17a1c5ec9156f2b31dfae57a6217
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 20:47:18 -0500

    practice with NOT and OR

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -534,3 +534,16 @@ function add(str) {
 function nand(value1, value2) {
     return !(value1 && value2);
 }
+
+
+
+
+
+//Write a function nor that takes two Boolean values. If both values are false,
+// the result should be true. In the other cases the return should be false.
+// I.e.: The call nor(false, false) should return true. The calls nor(true, false),
+// nor(false, true) and nor(true, true) should return false.
+
+function nor(value1, value2) {
+    return !(value1 || value2);
+}
