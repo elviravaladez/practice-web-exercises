abe2bb447ab5f6c8717f948328fb8c9203228cf3
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-12 18:04:12 -0500

    completed the farm problem

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -804,3 +804,27 @@ function helloWorld(num) {
 function reverse(arr) {
     return arr.reverse();
 }
+
+
+
+//The Farm Problem
+
+//In this challenge, a farmer is asking you to tell him how
+// many legs can be counted among all his animals.
+// The farmer breeds three species:
+
+//chickens = 2 legs
+// cows = 4 legs
+// pigs = 4 legs
+
+//The farmer has counted his animals and he gives you a subtotal
+// for each species. You have to implement a function that returns
+// the total number of legs of all the animals.
+
+//EXAMPLES:
+//animals(2, 3, 5) ➞ 36
+// animals(1, 2, 3) ➞ 22
+// animals(5, 2, 8) ➞ 50
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4);
+}
