d9bc07b40dd469d6eb2566a051ca7a420c397055
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 20:44:51 -0500

    practice with booleans

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -514,3 +514,23 @@ function add(str) {
     var num2 = parseInt(str.substring(plusSign));
     return num1 + num2;
 }
+
+
+
+
+
+
+
+
+
+
+/******* BOOLEAN ********/
+
+//Write a function nand that takes two Boolean values. If both values are true,
+// the result should be false. In the other cases the return should be true.
+// I.e.: The call nand(true, true) should return false. The calls nand(true, false),
+// nand(false, true) and nand(false, false) should return true.
+
+function nand(value1, value2) {
+    return !(value1 && value2);
+}
