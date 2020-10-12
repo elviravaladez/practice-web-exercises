c3d79dc7e246193083c9409f0bb409192172035e
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 21:33:44 -0500

    Luke, I Am Your..

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -643,3 +643,27 @@ function frames(minutes, fps) {
 
 
 
+//Luke, I Am Your ...
+
+//Luke Skywalker has family and friends. Help him remind
+// them who is who. Given a string with a name, return
+// the relation of that person to Luke.
+
+//EXAMPLE:
+//relationToLuke("Darth Vader") ➞ "Luke, I am your father."
+// relationToLuke("Leia") ➞ "Luke, I am your sister."
+// relationToLuke("Han") ➞ "Luke, I am your brother in law."
+
+function relationToLuke(name) {
+    if(name === "Darth Vader") {
+        return "Luke, I am your father.";
+    } else if(name === "Leia") {
+        return "Luke, I am your sister.";
+    } else if(name === "Han") {
+        return "Luke, I am your brother in law.";
+    } else if(name === "R2D2") {
+        return "Luke, I am your droid.";
+    } else {
+        return "Luke, I am not related to you"
+    }
+}
