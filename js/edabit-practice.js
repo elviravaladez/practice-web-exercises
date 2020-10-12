d01bb1fd9d7b6f5eb8330aeea2fa00cd74339f22
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 21:41:37 -0500

    adding dividesEvenly function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -689,3 +689,13 @@ function stringInt(str) {
 function divisibleByFive(n) {
     return n % 5 === 0;
 }
+
+
+
+//Divides Evenly
+
+//Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+
+function dividesEvenly(a, b) {
+    return a % b === 0;
+}
