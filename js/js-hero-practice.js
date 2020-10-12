4ea5c25329784eb3094944b5040fb64a28531c57
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 20:56:44 -0500

    practice with NOT

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -547,3 +547,14 @@ function nand(value1, value2) {
 function nor(value1, value2) {
     return !(value1 || value2);
 }
+
+
+
+//Write a function xor that takes two Boolean values. If both values are different,
+// the result should be true. If both values are the same, the result should be false.
+// I.e.: The calls xor(true, false) and xor(false, true) should return true. The calls
+// xor(true, true) and xor(false, false) should return false.
+
+function xor(value1, value2) {
+    return !value1 === value2;
+}
