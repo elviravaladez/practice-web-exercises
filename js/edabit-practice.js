42a3e0eb0e671edfedc0294397daf1ec23e220d5
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 21:47:13 -0500

    adding pair management exercise

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -709,3 +709,17 @@ function dividesEvenly(a, b) {
 function length(str) {
     return str.length;
 }
+
+
+
+//Pair Management
+
+//Given two arguments, return an array which contains these two arguments.
+//EXAMPLES:
+//makePair(1, 2) ➞ [1, 2]
+// makePair(51, 21) ➞ [51, 21]
+// makePair(512124, 215) ➞ [512124, 215]
+
+function makePair(num1, num2) {
+    return [num1, num2];
+}
