6f028a0dc603244386fad23470b1ce60b323c313
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-11 21:27:09 -0500

    completed frames function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -626,3 +626,20 @@ function calcAge(age) {
 
 
 
+//Frames Per Second
+
+//Create a function that returns the number of frames shown in
+// a given number of minutes for a certain FPS.
+
+//EXAMPLES:
+//frames(1, 1) ➞ 60
+// frames(10, 1) ➞ 600
+// frames(10, 25) ➞ 15000
+
+function frames(minutes, fps) {
+    return (minutes * fps) * 60;
+}
+
+
+
+
