a87cd233b56ca1844572f1dfa9c9272f0462108d
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-14 20:04:01 -0500

    adding volumeOfBox function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -894,3 +894,26 @@ function length(s) {
     }
     return total;
 }
+
+
+
+
+
+
+
+
+//Volume of a Box
+
+//Create a function that takes an object argument sizes
+// (contains width, length, height keys) and returns the
+// volume of the box.
+
+//EXAMPLES:
+//volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
+// volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
+// volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+
+function volumeOfBox(sizes) {
+    return sizes.width * sizes.height * sizes.length;
+}
+
