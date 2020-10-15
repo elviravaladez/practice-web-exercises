e577fb8babcfc3974fffb91d336b126058ab2162
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-15 10:20:35 -0500

    adding addOddToN function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -996,3 +996,18 @@ function isEmpty(obj) {
 function isEmpty(s) {
     return s === "";
 }
+
+
+//Sum of the Odd Numbers
+
+//Create a function which returns the total of all odd numbers
+// up to and including n. n will be given as an odd number.
+function addOddToN(n) {
+    var odd = 0;
+    for(var i = 0; i <= n; i++) {
+        if(i % 2 === 1) {
+            odd = odd + i;
+        }
+    }
+    return odd;
+}
