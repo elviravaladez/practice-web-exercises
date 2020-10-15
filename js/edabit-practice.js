4a4699e11afed64d54bf1b6e6fa3351eea628c79
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-14 20:46:09 -0500

    adding isEmpty

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -989,3 +989,10 @@ function isInRange(num, range) {
 function isEmpty(obj) {
     return Object.keys(obj).length === 0;
 }
+
+
+//Is the String Empty?
+//Create a function that returns true if a string is empty and false otherwise.
+function isEmpty(s) {
+    return s === "";
+}
