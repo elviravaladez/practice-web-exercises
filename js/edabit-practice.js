9587674b54b7e63d28375b7c4647bef3b3683cc4
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-16 21:12:01 -0500

    adding convert function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1013,3 +1013,15 @@ function addOddToN(n) {
 }
 
 
+//Convert Hours and Minutes into Seconds
+
+//Write a function that takes two integers (hours, minutes),
+// converts them to seconds, and adds them.
+//EXAMPLES
+//convert(1, 3) ➞ 3780
+// convert(2, 0) ➞ 7200
+// convert(0, 0) ➞ 0
+
+function convert(hours, minutes) {
+    return ((hours * 3600) + (minutes * 60));
+}
