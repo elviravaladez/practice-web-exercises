cc997f21744e04d4b1b3c10785d01a7fac1704de
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-16 22:56:27 -0500

    has_bugs function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1099,3 +1099,19 @@ function sortByLength(arr) {
         return a.length - b.length;
     });
 }
+
+
+//Buggy Code
+//The challenge is to try and fix this buggy code,
+// given the inputs true and false. See the examples
+// below for the expected output.
+
+//EXAMPLES:
+//has_bugs(true) ➞ "sad days"
+// has_bugs(false) ➞ "it's a good day"
+function has_bugs(buggy_code) {
+    if(buggy_code === true) {
+        return "sad days";
+    }
+    return "it's a good day";
+}
