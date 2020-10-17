bd404829c6dce5879cef659c95fde1a12de182bb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-16 21:52:59 -0500

    practice with Math.min and Math.max functions

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1025,3 +1025,23 @@ function addOddToN(n) {
 function convert(hours, minutes) {
     return ((hours * 3600) + (minutes * 60));
 }
+
+
+
+//Find the Smallest and Biggest Numbers
+
+//Create a function that takes an array of numbers and
+// return both the minimum and maximum numbers, in that order.
+
+//EXAMPLES
+//minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+// minMax([2334454, 5]) ➞ [5, 2334454]
+// minMax([1]) ➞ [1, 1]
+function minMax(arr) {
+    var min = Math.min(...arr);
+    var max = Math.max(...arr);
+    var newArr = [];
+    newArr.push(min);
+    newArr.push(max);
+    return newArr;
+}
