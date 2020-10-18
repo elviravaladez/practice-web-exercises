33ccdfbcd1fa645be8de82fd4c62709018c0465b
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-18 13:31:14 -0500

    practice with Math.ceil function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1561,3 +1561,29 @@ function numArgs() {
 function kineticEnergy(m, v) {
     return Math.round(((1 / 2) * m * Math.pow(v, 2)));
 }
+
+
+
+
+
+
+
+//Four Passengers and a Driver
+//A typical car can hold four passengers and one driver,
+// allowing five people to travel around. Given n
+// number of people, return how many
+// cars are needed to seat everyone comfortably.
+
+//EXAMPLES
+//carsNeeded(5) ➞ 1
+// carsNeeded(11) ➞ 3
+// carsNeeded(0) ➞ 0
+
+function carsNeeded(n) {
+    return Math.ceil(n / 5);
+}
+
+
+
+
+
