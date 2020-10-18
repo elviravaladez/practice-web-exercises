0f6f73c80852807bd9833ab630e3170654d2db4f
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 22:23:51 -0500

    repeating the same item multiple times in an array using push method

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1382,3 +1382,27 @@ function squaresSum(n) {
     }
     return total;
 }
+
+
+
+
+
+//Repeat the Same Item Multiple Times
+//Create a function that takes two arguments (item, times). The
+// first argument (item) is the item that needs repeating while the second
+// argument (times) is the number of times the item is to be repeated.
+// Return the result in an array.
+
+//EXAMPLES
+// repeat("edabit", 3) ➞ ["edabit", "edabit", "edabit"]
+// repeat(13, 5) ➞ [13, 13, 13, 13, 13]
+// repeat("7", 2) ➞ ["7", "7"]
+// repeat(0, 0) ➞ []
+
+function repeat(item, times) {
+    var arr = [];
+    for (var i = 0; i < times; i++) {
+        arr.push(item);
+    }
+    return arr;
+}
