3967a65d5bff7851d4b756c075579b00c9501bb4
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 20:58:52 -0500

    using substring() method to return a word without the first character

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1334,3 +1334,16 @@ function addUpTo(n) {
     }
     return total;
 }
+
+
+
+
+//Word without First Character
+//Create a function that takes a word and returns the new word without including the first character.
+//EXAMPLES
+//newWord("apple") ➞ "pple"
+// newWord("cherry") ➞ "herry"
+// newWord("plum") ➞ "lum"
+function newWord(str) {
+    return str.substring(1);
+}
