a1aac0aeb0be4a2cd750636879ec544cd4903788
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 22:34:58 -0500

    finding the index using indexOf method

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1425,3 +1425,19 @@ function repeat(item, times) {
 function isOdd(num) {
     return num % 2 === 1 || num % 2 === -1;
 }
+
+
+//Find the Index (Part #2)
+//Create a function that searches for the index of a given
+// item in an array. If the item is present, it should return
+// the index, otherwise, it should return -1.
+
+
+//EXAMPLES:
+
+//search([1, 2, 3, 4], 3) ➞ 2
+// search([2, 4, 6, 8, 10], 8) ➞ 3
+// search([1, 3, 5, 7, 9], 11) ➞ -1
+function search(arr, item) {
+    return arr.indexOf(item);
+}
