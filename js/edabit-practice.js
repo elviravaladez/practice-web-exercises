0984dbc5465afc3cdfe4bec7a248f2a36b05540c
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 19:57:47 -0500

    adding function addUp

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1115,3 +1115,24 @@ function has_bugs(buggy_code) {
     }
     return "it's a good day";
 }
+
+
+
+//Add up the Numbers from a Single Number
+
+//Create a function that takes a number as an argument.
+// Add up all the numbers from 1 to the number you passed
+// to the function. For example, if the input is 4 then
+// your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+//EXAMPLES:
+//addUp(4) ➞ 10
+// addUp(13) ➞ 91
+// addUp(600) ➞ 180300
+function addUp(num) {
+    var total = 0;
+    for(var i = 0; i <= num; i++) {
+        total += i;
+    }
+    return total;
+}
