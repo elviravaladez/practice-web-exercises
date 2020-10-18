a9764e9d71c163581cb14ff92a9c8f61549063eb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-18 14:55:52 -0500

    adding starNumber function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1827,3 +1827,25 @@ const helloName = name => `Hello ${name}!`;
 function check(arr, el) {
     return arr.includes(el);
 }
+
+
+
+
+
+//Nth Star Number
+//Create a function that takes a positive integer and returns
+// the nth "star number".
+
+// A star number is a centered figurate number a centered
+// hexagram (six-pointed star), such as the one that
+// Chinese checkers is played on.
+
+//EXAMPLES
+//starNumber(2) ➞ 13
+// starNumber(3) ➞ 37
+// starNumber(5) ➞ 121
+
+
+function starNumber(n) {
+    return (6 * n * (n - 1) + 1); ;
+}
