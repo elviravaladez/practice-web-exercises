f9ea725f47256771922255d9006f0bef207ff782
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-18 13:59:12 -0500

    practice with pop and shift methods

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1635,3 +1635,41 @@ function posCom(num) {
 function numberArgs(arg) {
     return arguments.length;
 }
+
+
+//Similar Bread
+//Given two arrays, which represent two sandwiches, return whether
+// both sandwiches use the same type of bread. The bread will
+// always be found at the start and end of the array.
+
+//The arrays will always be three elements long.
+
+//The first piece of bread on one sandwich must be the same as
+// the first piece of bread on the other sandwich. The same goes
+// for the last piece of bread.
+
+//EXAMPLES:
+//hasSameBread(
+//   ["white bread", "lettuce", "white bread"],
+//   ["white bread", "tomato", "white bread"]
+// ) ➞ true
+//
+// hasSameBread(
+//   ["brown bread", "chicken", "brown bread"],
+//   ["white bread", "chicken", "white bread"]
+// ) ➞ false
+//
+// hasSameBread(
+//   ["toast", "cheese", "toast"],
+//   ["brown bread", "cheese", "toast"]
+// ) ➞ false
+function hasSameBread(arr1, arr2) {
+    var firstArr1 = arr1.shift();
+    var lastArr1 = arr1.pop();
+    var firstArr2 = arr2.shift();
+    var lastArr2 = arr2.pop();
+    if(firstArr1 === firstArr2 && lastArr1 === lastArr2) {
+        return true;
+    }
+    return false;
+}
