d67083a439f1f5598f8cc1413d7d1b3b8183de5b
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 22:43:09 -0500

    solving for how many sticker on a Rubik's cube

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1457,3 +1457,20 @@ function search(arr, item) {
 function hasSpaces(str) {
     return str.includes(" ");
 }
+
+//Number of Stickers
+//Given a Rubik's Cube with a side length of n, return the number of
+// individual stickers that are needed to cover the whole cube.
+//Keep in mind there are 6 faces to keep track of.
+
+//The Rubik's cube of side length 1 has 6 stickers.
+// The Rubik's cube of side length 2 has 24 stickers.
+// The Rubik's cube of side length 3 has 54 stickers.
+
+//EXAMPLES
+//howManyStickers(1) ➞ 6
+// howManyStickers(2) ➞ 24
+// howManyStickers(3) ➞ 54
+function howManyStickers(n) {
+    return Math.pow(n, 2) * 6;
+}
