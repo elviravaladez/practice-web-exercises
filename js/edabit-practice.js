d89069021ec877d2a7a90e9b63275301b4085e63
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-17 23:13:43 -0500

    adding stackBoxes function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -1489,3 +1489,20 @@ function tuckIn(arr1, arr2) {
     concat.push(lastNum);
     return concat;
 }
+
+
+
+
+//Stack the Boxes
+// Write a function that takes a number n and returns the number of stacked boxes
+// in a model n levels high, visible and invisible.
+
+
+//EXAMPLES
+//stackBoxes(1) ➞ 1
+// stackBoxes(2) ➞ 4
+// stackBoxes(0) ➞ 0
+
+function stackBoxes(n) {
+    return Math.pow(n, 2);
+}
