d7e930d57226fba8cc471f9670da2fec9ed49470
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-21 18:58:20 -0500

    refactored nSidedShape function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2065,3 +2065,22 @@ function nSidedShape(n) {
             break;
     }
 }
+
+
+
+//Refactored Switch Statement
+function nSidedShape(n) {
+    var shapes = {
+        1:	"circle",
+        2:	"semi-circle",
+        3:	"triangle",
+        4:	"square",
+        5:	"pentagon",
+        6:	"hexagon",
+        7:	"heptagon",
+        8:	"octagon",
+        9:	"nonagon",
+        10:	"decagon",
+    }
+    return shapes[n];
+}
