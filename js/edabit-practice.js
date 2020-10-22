51c1b9d4ced3f56d3f1c0bb0c8893c97180a62dd
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-21 19:29:18 -0500

    finding the largest number in an array

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2131,3 +2131,16 @@ function halfQuarterEighth(n) {
     newArr.push(eighth);
     return newArr;
 }
+
+
+//Find the Largest Number in an Array
+//Create a function that takes an array of
+// numbers. Return the largest number in the array.
+
+//EXAMPLES
+//findLargestNum([4, 5, 1, 3]) ➞ 5
+// findLargestNum([300, 200, 600, 150]) ➞ 600
+// findLargestNum([1000, 1001, 857, 1]) ➞ 1001
+function findLargestNum(arr) {
+    return Math.max(...arr);
+}
