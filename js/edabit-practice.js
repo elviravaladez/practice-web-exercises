a80e852c900327985fa6e2cdeed249f499a7f3ed
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-21 19:40:59 -0500

    filtering strings from an array

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2144,3 +2144,18 @@ function halfQuarterEighth(n) {
 function findLargestNum(arr) {
     return Math.max(...arr);
 }
+
+
+
+//Filter Strings from Array
+//Create a function that takes an array of strings and
+// numbers, and filters out the array so that it
+// returns an array of integers only.
+
+//EXAMPLES
+//filterArray([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
+// filterArray(["A", 0, "Edabit", 1729, "Python", "1729"]) ➞ [0, 1729]
+// filterArray(["Nothing", "here"]) ➞ []
+function filterArray(arr) {
+    return arr.filter(Number.isInteger);
+}
