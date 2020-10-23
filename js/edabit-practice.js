c5f9c838ed58955ab9c4dbb97ea758d2941cfa49
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 17:47:13 -0500

    adding diffMaxMin funciton

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2310,3 +2310,20 @@ function sortDrinkByPrice(drinks){
     });
     return sorted;
 }
+
+
+//Difference of Max and Min Numbers in Array
+//Create a function that takes an array and returns the
+// difference between the biggest and smallest numbers.
+//EXAMPLES
+//diffMaxMin([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+// // Smallest number is -50, biggest is 32.
+//
+// diffMaxMin([44, 32, 86, 19]) ➞ 67
+// // Smallest number is 19, biggest is 86.
+
+function diffMaxMin(arr) {
+    var max = Math.max(...arr);
+    var min = Math.min(...arr);
+    return max - min;
+}
