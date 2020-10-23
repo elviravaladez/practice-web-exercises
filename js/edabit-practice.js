58da71095e46d51154638c7f46a76a23d4bcc1e8
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 15:47:07 -0500

    adding profit function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2252,3 +2252,37 @@ function createID(firstname, lastname) {
 function createID(firstname, lastname) {
     return firstname[0].toLowerCase() + lastname[0].toUpperCase() + lastname[1].toLowerCase() + lastname[2].toLowerCase();
 }
+
+
+//Calculate the Profit
+//You work for a manufacturer, and have been asked to calculate the total
+// profit made on the sales of a product. You are given an object
+// containing the cost price per unit (in dollars), sell price per
+// unit (in dollars), and the starting inventory. Return the total
+// profit made, rounded to the nearest dollar.
+
+//EXAMPLES
+//profit({
+//   costPrice: 32.67,
+//   sellPrice: 45.00,
+//   inventory: 1200
+// }) ➞ 14796
+//
+//
+// profit({
+//   costPrice: 225.89,
+//   sellPrice: 550.00,
+//   inventory: 100
+// }) ➞ 32411
+//
+//
+// profit({
+//   costPrice: 2.77,
+//   sellPrice: 7.95,
+//   inventory: 8500
+// }) ➞ 44030
+function profit(info) {
+    var totalCost = info.costPrice * info.inventory;
+    var totalSales = info.sellPrice * info.inventory;
+    return Math.round(totalSales - totalCost);
+}
