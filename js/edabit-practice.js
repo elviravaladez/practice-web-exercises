ef390f6109816393860841e1456b5a0d60b3655b
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 15:16:25 -0500

    function sayWhat

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2204,3 +2204,24 @@ function addIndexes(arr) {
     }
     return arr;
 }
+
+
+//Burglary Series (11): Say What
+//The insurance guy calls again. Apparently, they were informed by
+// your spouse that some items were not stolen at all and you failed
+// to mention this detail to them. This is a fraud attempt! You freeze
+// and mumble something unintelligible. Find out what you said.
+//
+// Given an object with always the exact same 3 keys, return a
+// string that concatenates all the values and adds the 2nd key
+// at the end. Make sure you keep an empty space between them but
+// not at the beginning or end of the string. Look at the examples
+// for a clearer picture.
+
+//EXAMPLES
+//{ 1: "Mommy", 2: "please", 3: "help" } ➞ "Mommy please help please"
+// { 1: "Me", 2: "innocent", 3: "is" } ➞ "Me innocent is innocent"
+// { 1: "Must", 2: "lawyer", 3: "call" } ➞ "Must lawyer call lawyer"
+function sayWhat(obj) {
+    return obj["1"] + " " + obj["2"] + " " + obj["3"] + " " + obj["2"];
+}
