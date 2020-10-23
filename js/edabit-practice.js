e9d68f3932435c462cb3df61e1c56cab94298c45
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 15:29:34 -0500

    adding createID function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2225,3 +2225,25 @@ function addIndexes(arr) {
 function sayWhat(obj) {
     return obj["1"] + " " + obj["2"] + " " + obj["3"] + " " + obj["2"];
 }
+
+//City School Creating IDS
+//Many IDS (for emails or Google ID) are created using the person's name.
+//
+// Create a function that will return a four-character ID using
+// the person's first name and last name. The first character will
+// be the first letter of the first name but in lowercase. The
+// next three characters will be the first three characters of the
+// last name, but the first letter will be capitalized
+// and the other two will be in lower case.
+
+
+//EXAMPLES
+//createID("mary", "lamb") ➞ "mLam"
+// createID("John", "SMITH") ➞ "jSmi"
+// createID("mary", "smith") ➞ "mSmi"
+function createID(firstname, lastname) {
+    var firstInitial = firstname.charAt(0).toLowerCase();
+    var lastInitialCap = lastname.charAt(0).toUpperCase();
+    var lastTwoLetters = lastname.slice(1, 3).toLowerCase();
+    return firstInitial + lastInitialCap + lastTwoLetters;
+}
