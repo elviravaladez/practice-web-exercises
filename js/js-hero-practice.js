921170fa79db88d8e5bd0478598d2ee1c438fd6f
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 22:57:07 -0500

    sorting items in an array

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -773,3 +773,14 @@ function setFirstElement(arr, num) {
 function getLastElement(arr) {
     return arr.pop();
 }
+
+
+
+//Write a function sort that takes an array filled
+// with 3 numbers and returns these 3 numbers sorted
+// in ascending order as an array.
+//
+// Example: sort([2, 3, 1]) should return [1, 2, 3].
+function sort(arr) {
+    return arr.sort()
+}
