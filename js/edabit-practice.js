9485b14f551d38644b84e97a1320f904dcb2eabc
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-24 00:59:16 -0500

    adding longBurp function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2423,3 +2423,19 @@ function intOrString(param) {
         return "str";
     }
 }
+
+
+
+//Burrrrrrrp
+//Create a function that returns the string "Burp"
+// with the amount of "r's" determined by the input
+// parameters of the function.
+//EXAMPLES
+//longBurp(3) ➞ "Burrrp"
+// longBurp(5) ➞ "Burrrrrp"
+// longBurp(9) ➞ "Burrrrrrrrrp"
+
+function longBurp(num) {
+	var r = "r";
+	return "Bu" + r.repeat(num) + "p";
+}
