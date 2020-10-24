38578ce0abd0dd9be2fe8416b41d339cb6ad8f30
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 23:12:13 -0500

    add function

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -797,3 +797,16 @@ function rotate(arr) {
     arr.push(a);
     return arr;
 }
+
+
+//Write a function add that adds an element to the end of an array.
+// However, the element should only be added if it is
+// not already in the array.
+//
+// Example: add([1, 2], 3) should return [1, 2, 3] and add([1, 2], 2) should return [1, 2].
+function add(arr, num) {
+    if(arr.indexOf(num) === -1) {
+        arr.push(num);
+    }
+    return arr;
+}
