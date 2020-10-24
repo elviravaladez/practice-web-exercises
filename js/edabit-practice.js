07886f5969d57e65924ab0e0e611af074befee79
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-24 00:18:25 -0500

    function circle or square

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2345,3 +2345,20 @@ function toScottishScreaming(str) {
     var vowels = /[AIOU]/gi;
     return str.replace(vowels, 'e').toUpperCase();
 }
+
+
+
+//Circle or Square
+//Given the radius of a circle and the area of a square, return
+// true if the circumference of the circle is greater than the
+// square's perimeter and false if the square's perimeter is
+// greater than the circumference of the circle.
+//EXAMPLES
+//circle_or_square(16, 625) ➞ True
+//
+// circle_or_square(5, 100) ➞ False
+//
+// circle_or_square(8, 144) ➞ True
+function circle_or_square(rad, area){
+    return ( 2 * Math.PI * rad) > (4 * Math.sqrt(area));
+}
