36fcbdb3fd6d96224300bb2f29c2f0a7be26d14e
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 21:01:47 -0500

    adding isThreeDigit function

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -648,3 +648,10 @@ function unequal(value1, value2, value3) {
 
 
 /******* COMPARE NUMBERS *******/
+//Write a function isThreeDigit that checks if a number
+// is greater than or equal to 100 and less than 1000.
+
+//Example: isThreeDigit(500) should return true and isThreeDigit(50) should return false.
+function isThreeDigit(num) {
+    return num >= 100 && num < 1000;
+}
