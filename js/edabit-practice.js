c977282ecdbbcac9cb97b2ba11bc87e535be9982
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-24 01:10:45 -0500

    using split and join to add spaces between each character

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2455,3 +2455,15 @@ function getMultipliedArr(arr) {
     }
     return arr;
 }
+
+
+//Spaces Between Each Character
+//Create a function that takes a string and returns a
+// string with spaces in between all of the characters.
+//EXAMPLES:
+//spaceMeOut("space") ➞ "s p a c e"
+// spaceMeOut("far out") ➞ "f a r  o u t"
+// spaceMeOut("elongated musk") ➞ "e l o n g a t e d   m u s k"
+function spaceMeOut(str) {
+    return str.split("").join(" ");
+}
