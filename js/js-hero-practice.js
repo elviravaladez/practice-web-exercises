389706093f04d036c1fb75a0351c83fc022bb048
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-23 22:52:07 -0500

    adding setFirstElement function

--- js/js-hero-practice.js
+++ js/js-hero-practice.js
@@ -751,3 +751,16 @@ function toArray(value1, value2) {
 function getFirstElement(arr) {
     return arr.shift();
 }
+
+
+
+//Write a function setFirstElement that takes an array and an
+// arbitrary variable. The variable should be inserted as
+// the first element in the array. The array should be returned.
+//
+// Example: setFirstElement([1, 2], 3) should return [3, 2].
+function setFirstElement(arr, num) {
+    arr.shift();
+    arr.unshift(num);
+    return arr;
+}
