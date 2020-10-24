2d2246e1851bbf9c0e9501a3a47915af6ef642e2
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-10-24 01:16:16 -0500

    adding volPizza function

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2467,3 +2467,26 @@ function getMultipliedArr(arr) {
 function spaceMeOut(str) {
     return str.split("").join(" ");
 }
+
+
+//Volume of a Pizza
+//Tom is a very methodic guy that loves geometry and pizza:
+// he loves them so much that, before eating a pizza, he
+// calculates its radius and its height. Now, he wants to
+// know from you the total volume of pizza that he swallowed!
+//
+// You are given the two parameters that Tom measured: radius height
+
+//He tells you that if you multiply the height for the square of the radius and
+// multiply the result for the mathematical constant π (Pi), you will obtain the total volume of the pizza. Implement a function that returns the volume of the
+// pizza as a whole number, rounding it to the nearest integer (and rounding up
+// for numbers with .5 as decimal part).
+
+//EXAMPLES
+//volPizza(1, 1) ➞ 3
+// // (radius² x height x π) = 3.14159... rounded to the nearest integer.
+// volPizza(7, 2) ➞ 308
+// volPizza(10, 2.5) ➞ 785
+function volPizza(radius, height) {
+    return Math.round(Math.pow(radius, 2) * height * Math.PI);
+}
