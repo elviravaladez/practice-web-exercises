586771b51f7b19409ed1ee662d5c9e295c1d47d3
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-11-21 13:41:02 -0600

    ES6: Destructuring Arrays I

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2507,3 +2507,21 @@ function countVowels(str) {
 function countVowels(str) {
     return str.match(/[aeiou]/gi).length;
 }
+
+
+//ES6: Destructuring Arrays I
+//You can assign variables from arrays like this:
+
+//const arr = [1, 2, 3, 4, 5, 6]
+// let a = arr[0]
+// let b = arr[1]
+//
+// console.log(a) // outputs 1
+// console.log(b) // outputs 2
+
+
+//With ES6, you can assign variables from arrays in a much
+// more succinct way. Create variables a and b from the given
+// array using the ES6 destructuring assignment syntax, where a === 1 and b === 2.
+const arr = [1, 2, 3, 4, 5, 6];
+const [a, b] = arr;
