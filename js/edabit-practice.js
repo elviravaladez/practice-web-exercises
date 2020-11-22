c055bad5d9a48acbddecaabbd1a648af5dc3ac30
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-11-21 21:00:50 -0600

    ES6: Destructuring Arrays II

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2525,3 +2525,34 @@ function countVowels(str) {
 // array using the ES6 destructuring assignment syntax, where a === 1 and b === 2.
 const arr = [1, 2, 3, 4, 5, 6];
 const [a, b] = arr;
+
+
+
+//ES6: Destructuring Arrays II
+//You can assign variables from nested arrays like this:
+
+//const arr = ["cars", "planes", ["trains", ["motorcycles"]]]
+//
+// let trans1 = arr[0]
+// let trans2 = arr[1]
+// let trans3 = arr[2][0]
+// let trans4 = arr[2][1][0]
+//
+// console.log(trans1) // outputs "cars"
+// console.log(trans2) // outputs "planes"
+// console.log(trans3) // outputs "trains"
+// console.log(trans4) // outputs "motorcycles"
+
+//With ES6, you can assign variables from arrays in a much more succinct way.
+const arr = ["cars", "planes", ["trains", ["motorcycles"]]]
+
+// Fix the following using es6 destructuring
+// Only edit what's inside of [ trans1, trans2, trans3, trans4 ]
+let [ trans1, trans2, [trans3, [trans4]]] = arr;
+
+/*
+console.log(trans1) // should output "cars"
+console.log(trans2) // should output "planes"
+console.log(trans3) // should output "trains"
+console.log(trans4) // should output "motorcycles"
+*/
