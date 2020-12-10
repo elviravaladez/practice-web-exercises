0ded99b4dec200799a7d13c2a62ef87f51256998
Elvira Valadez <elvira.marie.valadez@gmail.com>
2020-12-09 19:10:18 -0600

    Array Indexing: Returning the value of the array w/the given index

--- js/edabit-practice.js
+++ js/edabit-practice.js
@@ -2603,3 +2603,16 @@ function wumbo(words) {
     const M = /M/gi;
     return words.toUpperCase().replace(M, "W");
 }
+
+//Array Indexing
+//Given an index and an array, return the value of the array with the given index.
+
+//EXAMPLES:
+//valueAt([1, 2, 3, 4, 5, 6], 10 / 2) ➞ 6
+//
+// valueAt([1, 2, 3, 4, 5, 6], 8.0 / 2) ➞ 5
+//
+// valueAt([1, 2, 3, 4], 6.535355314 / 2) ➞ 4
+function valueAt(arr, i) {
+    return arr[Math.floor(i)];
+}
