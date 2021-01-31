40efb7590188cea5d42ddad3b9ecc9dc858ba8b9
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-01-30 22:16:36 -0600

    power function

--- js/big-o.js
+++ js/big-o.js
@@ -11,4 +11,16 @@
     }
 
 
+    //O(N) -> The recursive code iterates through N calls, since it subtracts one at each level
+    function power(a, b) {
+        if(b < 0) {
+            return 0;
+        } else if(b === 0) {
+            return 1;
+        } else {
+            return a * power(a, b -1); // recursive
+        }
+    }
+
+
 })();
