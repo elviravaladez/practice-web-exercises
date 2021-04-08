5f7d63434a58f8d232407956ecda667500efc7bb
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-07 23:32:17 -0500

    add: comp function

--- js/data-structures-and-algorithms.js
+++ js/data-structures-and-algorithms.js
@@ -156,10 +156,7 @@
     }
 
 
-
-
     //TODO: Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
-
     function comp(array1, array2){
         if(array1 === null || array2 === null) {
             return false;
