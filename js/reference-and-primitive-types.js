8905a2712284f3815e83b28107b5d52b155c60c8
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-16 14:36:06 -0500

    add: map array function

--- js/reference-and-primitive-types.js
+++ js/reference-and-primitive-types.js
@@ -45,3 +45,14 @@ console.log(secondPerson);
 //const thePerson = {
 //     name: 'Jane'
 // };
+
+
+//ARRAY FUNCTIONS
+const numbers = [1, 2, 3];
+
+const doubleNumArray = numbers.map((num) => {
+    return num * 2;
+});
+
+console.log(numbers); // [1, 2, 3]
+console.log(doubleNumArray); // [2, 4, 6]
