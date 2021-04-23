58ea0fce4daaf5be3f54f100fae2ae7e4e4e77df
Elvira Valadez <elvira.marie.valadez@gmail.com>
2021-04-22 23:36:45 -0500

    add: isSubsequence

--- js/data-structures-and-algorithms.js
+++ js/data-structures-and-algorithms.js
@@ -396,4 +396,22 @@
         }
         return true;
     }
+
+    // Write a function called "isSubsequence" which takes in two strings and checks whether the characters in the first string form a subsequence of the characters in teh second string. In other words, the function should check whether the characters in the first string appear somewhere in the second string, without their order changing.
+    function isSubsequence(str1, str2) {
+        let i = 0;
+        let j = 0;
+        while(i < str1.length){
+            if(j === str2.length) {
+                return false;
+            }
+
+            if(str1[i] === str2[j]){
+                i++;
+            }
+            j++;
+        }
+
+        return true;
+    }
 })();
